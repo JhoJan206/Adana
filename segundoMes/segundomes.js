2b766097a8857dc37b8ceebc03d0012b36dc2d69
JhoJan206 <jacomejhojan2006@gmail.com>
2025-06-19 17:56:56 -0500

    Solucion menu

--- segundoMes/segundomes.js
+++ segundoMes/segundomes.js
@@ -1,28 +1,25 @@
 $(document).ready(function() {
-    
     var envelope = $('#envelope');
-    var btn_open = $("#open");
-    var btn_reset = $("#reset");
-    
-    envelope.click( function() {
+    var btn_open = $('#open');
+    var btn_reset = $('#reset');
+
+    envelope.click(function() {
         open();
     });
-    btn_open.click( function() {
+    btn_open.click(function() {
         open();
     });
-    btn_reset.click( function() {
+    btn_reset.click(function() {
         close();
     });
 
     function open() {
-        envelope.addClass("open")
-           .removeClass("close");
-        $('.message').addClass("animated fadeInUp"); // Adding animation class
+        envelope.addClass("open").removeClass("close");
+        $('.message').addClass("animated fadeInUp");
     }
+
     function close() {
-        envelope.addClass("close")
-           .removeClass("open");
-        $('.message').removeClass("animated fadeInUp"); // Removing animation class
+        envelope.addClass("close").removeClass("open");
+        $('.message').removeClass("animated fadeInUp");
     }
-   
 });
